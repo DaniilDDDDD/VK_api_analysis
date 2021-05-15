3676d313339639c23ad83e780d5d707b7db77693
Andrew Zhukov <45205783+NixoN2@users.noreply.github.com>
2021-05-15 22:41:23 +0300

    Add files via upload

--- VK_api_analysis/api_plot/plots.py
+++ VK_api_analysis/api_plot/plots.py
@@ -77,13 +77,29 @@ def stats_plot(data, plot_type='bar'):
     plt.tight_layout()
     plt.savefig('media/current_plot.jpeg', dpi=225)
 
+def compare_hash_tags(hash_tag):
+    return hash_tag[1]
 
 def posts_analisis(data):
+    posts = []
+    items = data['items']
+    for item in items:
+        posts.append([item['text'], item['likes']['count']])
 
+    hash_tags = []
+    for post in posts:
+        hash_tag = set([string[1:] for string in post[0].split() if string.startswith("#")])
+        if hash_tag and not (hash_tag in hash_tags):
+            hash_tags.append([hash_tag, post[1]])
+
+    hash_tags.sort(key=compare_hash_tags)
 
     # сюда запиши результаты анализа в json формате
     result = {
 
     }
+
+    for hash_tag in hash_tags:
+        result[hash_tag[0]] = hash_tag[1]
 
     return result
